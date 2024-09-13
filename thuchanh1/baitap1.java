9e549a2cbc7e23f0582b69d0a626e924ff30552e
shengs1 <thinhnguyen081805@gmail.com>
2024-09-13 17:55:48 +0700

    Create baitap1.java

---
+++ thuchanh1/baitap1.java
@@ -0,0 +1,123 @@
+public class baitap1 {
+        public static void main(String[] args) {
+            drawDiamond();
+            System.out.println();
+            drawX();
+            System.out.println();
+            drawRocket();
+            System.out.println();
+        }
+        public static void drawDiamond() {
+            drawCone();
+            drawV();
+        }
+        public static void drawX() {
+            drawV();
+            drawCone();
+        }
+        public static void drawRocket() {
+            drawCone();
+            System.out.println();
+            drawBox();
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.print("  |  Viet|");System.out.print("    ");
+            System.out.println();
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.print("  |   Nam|");System.out.print("    ");
+            System.out.println();
+            drawBox();
+            drawCone();
+        }
+    
+        public static void drawCone() {
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.print("     /\\");System.out.print("       ");
+            System.out.println();
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.print("    /  \\");System.out.print("      ");
+            System.out.println();
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.print("   /    \\");System.out.print("     ");
+            System.out.println();
+        }
+        public static void drawV() {
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.print("   \\    /");System.out.print("     ");
+            System.out.println();
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.print("    \\  /");System.out.print("      ");
+            System.out.println();
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.print("     \\/");System.out.print("       ");
+            System.out.println();
+        }
+        public static void drawBox() {
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.println();
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.println();
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.print("  |      |");System.out.print("    ");
+            System.out.println();
+            
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.print("  +------+");System.out.print("    ");
+            System.out.println();
+        }
+        public static void fivehinh() {
+            drawDiamond();
+            drawX();
+            drawRocket();  
+        }
+    
+    }
