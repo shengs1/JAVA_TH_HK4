daf76a37b90f8f954922686d511830e3c105e85a
risks1 <thinhnguyen081805@gmail.com>
2024-09-20 17:14:41 +0700

    Update thuchanh7.java

--- thuchanh2/thuchanh7.java
+++ thuchanh2/thuchanh7.java
@@ -1,30 +1,51 @@
 public class thuchanh7 {
     public static void main(String[] args) {
         int i,j;
+        System.out.println("+---------+");
         for( i = 1; i <= 5; i++)
         {
-            for(j = 1; j <=(i-1); j++)
+            System.out.print("|");
+            for(j = 1; j<=(i-1); j++)
             {
                 System.out.print(" ");
             }
+
             for (j=1;j<=(11-2*i);j++)
             {
                 System.out.print("*");
             }
+
+            for(j = 1; j<=(i-1); j++)
+            {
+                System.out.print(" ");
+            }
+
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("+---------+");
 
         for( i = 5; i >= 1; i--)
         {
+            System.out.print("|");
             for(j = 1; j <=(i-1); j++)
             {
                 System.out.print(" ");
             }
+
             for (j=1;j<=(11-2*i);j++)
             {
                 System.out.print("*");
             }
+
+            for(j = 1; j<=(i-1); j++)
+            {
+                System.out.print(" ");
+            }
+
+            System.out.print("|");
             System.out.println();
         }
+        System.out.println("+---------+");
     }
 }
