314bf79bb0ee2e290158abfd92561703e29261f6
risks1 <thinhnguyen081805@gmail.com>
2024-09-20 16:04:44 +0700

    up

---
+++ thuchanh2/thuchanh1.java
@@ -0,0 +1,28 @@
+public class thuchanh1{
+    public static void main(String[] args) {
+        double hight;
+        double weight;
+        double bmi;
+
+        hight=70;
+        weight=195;
+        bmi= weight/(hight*hight) * 703;
+
+        System.out.println("Gia tri BMI1: ");
+        System.out.println(bmi);
+
+        //thuchanh2
+        //khoi tao gia tri ban dau
+        hight=70;
+        weight=195;
+        bmi= weight/(hight*hight) * 703;
+        
+        //hien thi ket qua
+        System.out.println("Gia tri BMI 2: ");
+        System.out.println(bmi);
+        System.out.println();
+        
+        //dùng nối chuỗi
+        System.out.println("Gia tri BMI 2: " + bmi);
+    }
+}
