7ff68fe6c4be569698ae9f3df859efb17a159f65
shengs1 <thinhnguyen081805@gmail.com>
2024-09-20 23:46:58 +0700

    Create btvn3.java

---
+++ btvn3.java
@@ -0,0 +1,23 @@
+public class btvn3 {
+    public static void main(String[] args) {
+        hinh1();
+        hinh2();
+        hinh1();
+        hinh2();
+        hinh1();
+    }
+    public static void hinh1() {
+        System.out.println();
+        System.out.println("+===+===+");
+    }
+    public static void hinh2() {
+        int i,j;
+        for(i = 1; i <= 3; i++){
+            System.out.println();
+            for(j = 1; j <= 3; j++){
+                System.out.print("|   ");
+            }
+        System.out.println();
+        }
+    }
+}
