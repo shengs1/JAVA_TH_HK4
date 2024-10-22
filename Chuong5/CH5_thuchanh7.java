da9145a18e025d8ae11d30443bd03365c1758ec9
shengs1 <147737349+shengs1@users.noreply.github.com>
2024-10-22 10:08:42 +0700

    Add files via upload

--- Chuong5/CH5_thuchanh7.java
+++ Chuong5/CH5_thuchanh7.java
@@ -6,24 +6,23 @@ public class CH5_thuchanh7 {
         Scanner console = new Scanner(System.in);
         System.out.println("Hay doan 1 con so tu 1-10");
         int dapan = console.nextInt();
+        console.nextLine(); 
 
         Random random = new Random();
         int solandoan = 0;
         int somaydoan = 0;
         String phanhoi;
         boolean dadoandung = false;
-        
 
-        while(!dadoandung){
+        while (!dadoandung) {
             somaydoan = random.nextInt(10) + 1;
             solandoan++;
 
             System.out.println("Co phai la so " + somaydoan + "? (y/n)");
             phanhoi = console.nextLine();
 
-            if(phanhoi.equalsIgnoreCase("y")){
+            if (phanhoi.equalsIgnoreCase("y")) {
                 dadoandung = true;
-
                 System.out.println("Toi da doan dung so lan cua ban sau " + solandoan + " lan");
             }
         }
