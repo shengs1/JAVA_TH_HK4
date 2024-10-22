88103b5d3903ec011c1560a38a17de29b4c36ddb
shengs1 <147737349+shengs1@users.noreply.github.com>
2024-10-22 09:51:05 +0700

    Add files via upload

--- Chuong5/CH5_thuchanh7.java
+++ Chuong5/CH5_thuchanh7.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class CH5_thuchanh7 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        System.out.println("Hay doan 1 con so tu 1-10");
+        int dapan = console.nextInt();
 
         Random random = new Random();
         int solandoan = 0;
         int somaydoan = 0;
         String phanhoi;
         boolean dadoandung = false;
-        System.out.println("Hay doan 1 con so tu 1-10");
+        
 
         while(!dadoandung){
             somaydoan = random.nextInt(10) + 1;
