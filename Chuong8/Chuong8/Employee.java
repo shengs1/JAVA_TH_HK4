047d3d8b22a6f86987802e1df8ee893d3cf3af51
shengs1 <147737349+shengs1@users.noreply.github.com>
2024-11-15 17:44:43 +0700

    Add files via upload

---
+++ Chuong8/Chuong8/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+
+    public int gethours() { // so h lam viec 1 tuan
+        return 40;
+    }
+
+    public double getsalary() { // luong nhan vien 40000$
+        return 40000.0;
+    }
+
+    public int getvacationdays() { // ngày nghỉ trong 1 nam toi da
+        return 14;
+    }
+
+    public String getvacationform() { 
+        return "yellow";
+    }
+}
---
+++ Chuong8/Chuong8/Secreatary.java
@@ -0,0 +1,24 @@
+public class Secreatary {
+    
+    public int gethours() { // so h lam viec 1 tuan
+        return 40;
+    }
+
+    public double getsalary() { // luong nhan vien 40000$
+        return 50000.0;
+    }
+
+    public int getvacationdays() { // ngày nghỉ trong 1 nam toi da
+        return 14;
+    }
+
+    public String getvacationform() { 
+        return "yellow";
+    }
+
+
+    public void hanhvirieng() {
+        System.out.println("Toi biet cach ghi chep.");
+    }
+
+}
